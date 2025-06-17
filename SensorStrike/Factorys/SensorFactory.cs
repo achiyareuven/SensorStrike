34860315d4e69e1a45e14ba4bccd7288c7ca2d8a
achiya reuven <achiya3144@gmail.com>
2025-06-17 11:58:55 +0300

    i addad factory for sensor and agant

---
+++ SensorStrike/Factorys/SensorFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SensorStrike.Enums;
+using SensorStrike.Sensors;
+
+namespace SensorStrike.Factorys
+{
+    public static class SensorFactory
+    {
+        public static ISensor CreaatSensor(SensorType sensorType)
+        {
+            switch(sensorType)
+            {
+                case SensorType.Audio:
+                    return new BaseSensor(SensorType.Audio);
+                case SensorType.Thermal:
+                    return new BaseSensor(SensorType.Thermal);
+                default: return null;
+
+            }
+        }
+    }
+}
