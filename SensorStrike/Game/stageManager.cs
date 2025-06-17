bdcc1ee5d0d225c5619ab6600d92d01f130d8836
achiya reuven <achiya3144@gmail.com>
2025-06-17 12:15:42 +0300

    הוספתי מחלקה לניהול שלבים

---
+++ SensorStrike/Game/stageManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SensorStrike.Agants;
+using SensorStrike.Factorys;
+using SensorStrike.Enums;
+
+namespace SensorStrike.Game
+{
+    public class StageManager
+    {
+        private List<IranAgent> _agants = new List<IranAgent> ();
+        private int _currentStage = 0;
+
+        public StageManager()
+        {
+            _agants.Add(AgentFactory.CreateAgent(AgentRank.FootSoldier, "7808990", "aly muahmad", "Revolutionary Guards"));
+        }
+        public bool IsGameOver() => _currentStage < _agants.Count;
+
+        public void NextAgant() => _currentStage++;
+
+
+        public IranAgent GetCurrentAgent()
+        {
+            return _agants [_currentStage];
+        }
+    }
+}
